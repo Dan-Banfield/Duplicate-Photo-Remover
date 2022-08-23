fb6d9a9b471b75cf9ac752e9ecb9099d5330582f
Dan Banfield <dandbanfield@gmail.com>
2022-08-23 18:09:38 +0100

    Add UI controls to main form.

--- Duplicate Photo Remover/Forms/MainForm.Designer.cs
+++ Duplicate Photo Remover/Forms/MainForm.Designer.cs
@@ -28,23 +28,225 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.selectedDirectoryTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.selectDirectoryButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.deleteDuplicateFilesCheckBox = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.statusProgressBar = new System.Windows.Forms.ProgressBar();
+            this.label8 = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(222, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Directory Properties";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Selected Directory";
+            // 
+            // selectedDirectoryTextBox
+            // 
+            this.selectedDirectoryTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selectedDirectoryTextBox.Location = new System.Drawing.Point(17, 75);
+            this.selectedDirectoryTextBox.Name = "selectedDirectoryTextBox";
+            this.selectedDirectoryTextBox.ReadOnly = true;
+            this.selectedDirectoryTextBox.Size = new System.Drawing.Size(351, 21);
+            this.selectedDirectoryTextBox.TabIndex = 2;
+            this.selectedDirectoryTextBox.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(240, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(214, 2);
+            this.label3.TabIndex = 3;
+            // 
+            // selectDirectoryButton
+            // 
+            this.selectDirectoryButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.selectDirectoryButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selectDirectoryButton.Location = new System.Drawing.Point(380, 74);
+            this.selectDirectoryButton.Name = "selectDirectoryButton";
+            this.selectDirectoryButton.Size = new System.Drawing.Size(75, 23);
+            this.selectDirectoryButton.TabIndex = 4;
+            this.selectDirectoryButton.TabStop = false;
+            this.selectDirectoryButton.Text = "...";
+            this.selectDirectoryButton.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(205, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(249, 2);
+            this.label4.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 111);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(187, 25);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Deletion Options";
+            // 
+            // deleteDuplicateFilesCheckBox
+            // 
+            this.deleteDuplicateFilesCheckBox.AutoSize = true;
+            this.deleteDuplicateFilesCheckBox.Checked = true;
+            this.deleteDuplicateFilesCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.deleteDuplicateFilesCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deleteDuplicateFilesCheckBox.Location = new System.Drawing.Point(17, 149);
+            this.deleteDuplicateFilesCheckBox.Name = "deleteDuplicateFilesCheckBox";
+            this.deleteDuplicateFilesCheckBox.Size = new System.Drawing.Size(169, 22);
+            this.deleteDuplicateFilesCheckBox.TabIndex = 7;
+            this.deleteDuplicateFilesCheckBox.Text = "Delete Duplicate Files";
+            this.deleteDuplicateFilesCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Enabled = false;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(17, 177);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(153, 22);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Delete Similar Files";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(97, 230);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(357, 2);
+            this.label6.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(12, 215);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(79, 25);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Status";
+            // 
+            // statusProgressBar
+            // 
+            this.statusProgressBar.Location = new System.Drawing.Point(17, 272);
+            this.statusProgressBar.Name = "statusProgressBar";
+            this.statusProgressBar.Size = new System.Drawing.Size(437, 23);
+            this.statusProgressBar.TabIndex = 11;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(14, 251);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(54, 16);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "READY";
+            // 
+            // startButton
+            // 
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(262, 315);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(93, 29);
+            this.startButton.TabIndex = 13;
+            this.startButton.TabStop = false;
+            this.startButton.Text = "START";
+            this.startButton.UseVisualStyleBackColor = true;
+            // 
+            // closeButton
+            // 
+            this.closeButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.closeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.closeButton.Location = new System.Drawing.Point(361, 315);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(93, 29);
+            this.closeButton.TabIndex = 14;
+            this.closeButton.TabStop = false;
+            this.closeButton.Text = "CLOSE";
+            this.closeButton.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(469, 504);
+            this.ClientSize = new System.Drawing.Size(469, 360);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.statusProgressBar);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.deleteDuplicateFilesCheckBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.selectDirectoryButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.selectedDirectoryTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Duplicate Photo Remover";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox selectedDirectoryTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button selectDirectoryButton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox deleteDuplicateFilesCheckBox;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ProgressBar statusProgressBar;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button closeButton;
     }
 }
 
